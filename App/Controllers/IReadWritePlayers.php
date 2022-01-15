4ec5528879bf62b2adb679f9e42c9876aea6efd2
teafoot <davidchenlog@gmail.com>
2022-01-15 08:54:33 -0800

    Adding Player Controller and Repository Interfaces

---
+++ App/Controllers/IReadWritePlayers.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+namespace App\Controllers;
+
+interface IReadWritePlayers {
+    function readPlayers($source, $filename = null): array;
+    function writePlayer($source, $player, $filename = null): void;
+    function display($isCLI, $source, $filename = null): void;
+}
+
